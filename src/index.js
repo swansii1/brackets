fdaa9233527da2190433a3360349c60553a1dec8
Matvey Lebedev <matvey.swansii@gmail.com>
2024-08-17 01:57:21 +0300

    completed the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openToClose = {};
+  const closeToOpen = {};
+
+  bracketsConfig.forEach(([open, close]) => {
+      openToClose[open] = close;
+      closeToOpen[close] = open;
+  });
+
+  const stack = [];
+
+  for (const char of str) {
+      if (openToClose[char]) {
+          if (char === openToClose[char] && stack.length > 0 && stack[stack.length - 1] === char) {
+              stack.pop();
+          } else {
+              stack.push(char);
+          }
+      } else if (closeToOpen[char]) {
+          if (stack.length === 0 || stack.pop() !== closeToOpen[char]) {
+              return false;
+          }
+      }
+  }
+
+  return stack.length === 0;
 }
